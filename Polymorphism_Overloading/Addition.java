3445d6a0128c249be5994cf9c203d138eaa05ed3
arjunabalpande06 <arjunabalpande@gmail.com>
2026-04-13 23:54:29 +0530

    Add files via upload

---
+++ Polymorphism_Overloading/Addition.java
@@ -0,0 +1,23 @@
+
+public class Addition {
+	public void add()
+	{
+		int a = 20,b = 30;
+		int c = a+b;
+		System.out.println("Addition is : "+c);
+	}
+	
+	public void add(int x)
+	{
+		int a = x,b = x;
+		int c = a+b;
+		System.out.println("Addition is : "+c);
+	}
+	
+	public void add(int x, int y)
+	{
+		int a = x,b = y;
+		int c = a+b;
+		System.out.println("Addition is : "+c);
+	}
+}
---
+++ Polymorphism_Overloading/AdditionMain.java
@@ -0,0 +1,16 @@
+
+public class AdditionMain {
+
+	public static void main(String[] args) {
+		Addition obj = new Addition();
+		obj.add();
+		System.out.println();
+		System.out.println();
+		obj.add(60);
+		System.out.println();
+		System.out.println();
+		obj.add(32, 52);
+
+	}
+
+}
